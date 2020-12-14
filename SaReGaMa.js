c998aca5f573ce64fde5a5c98fe8cdbab1443bff
DuosOptimos <73191449+DuosOptimos@users.noreply.github.com>
2020-12-14 10:18:39 +0530

    Add files via upload

---
+++ SaReGaMa.js
@@ -0,0 +1,19 @@
+function changebc(){
+    
+    document.getElementById("bodyy").style.background= colors[i]
+    
+    
+        
+    if (i==1)
+    {
+        i=2;
+    }
+    if (i==2)
+    {
+        i=1;
+    }
+}
+var colors = [
+     "blueviolet","cyan"
+]
+var i=1
